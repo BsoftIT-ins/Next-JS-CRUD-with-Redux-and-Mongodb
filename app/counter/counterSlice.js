f5bdd5a60047101b990ecbc45ec7778b9daf1e32
Azizul Islam <bsoftitinfo@gmail.com>
2024-09-26 23:22:41 +0600

    counter project done

--- app/counter/counterSlice.js
+++ app/counter/counterSlice.js
@@ -9,13 +9,20 @@ const counterSlice = createSlice({
     initialState:{
         count: 0,
     },
-    reducers: {},
+    reducers: {
+        increment: (state, action)=>{
+            state.count ++;
+        },
+        decrement: (state, action)=>{
+            state.count --;
+        },
+    },
 
 });
 
 // selector
-
+export const selectCount = (state) => state.counter;
 //action
-export const {} = counterSlice.reducer;
+export const { increment, decrement } = counterSlice.actions;
 //reducer
 export default counterSlice.reducer;
--- app/counter/page.jsx
+++ app/counter/page.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+"use client";
+
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, selectCount } from "./counterSlice";
 
 const CounterPage = () => {
+  const {count} = useSelector(selectCount);
+  const dispatch = useDispatch();
+  
   return (
     <div className='mx-20'>
-    <h1 className='text-9xl text-red-500'>0</h1>
-    <hr className='border-2'/>
-    <button className='text-7xl'>--</button>
-    <button className='text-7xl'>++</button>
-    
-    </div>
-  )
-}
+      <h1 className='text-9xl text-red-500'>{count}</h1>
+      <hr className='border-2' />
+      <button className='text-7xl bg-sky-400 mr-2 rounded-lg shadow-md hover:bg-sky-500 transition duration-300' onClick={() => dispatch(decrement())}>--</button>
+<button className='text-7xl bg-sky-400 rounded-lg shadow-md hover:bg-sky-500 transition duration-300' onClick={() => dispatch(increment())}>++</button>
 
-export default CounterPage
+    </div>
+  );
+};
+
+export default CounterPage;
